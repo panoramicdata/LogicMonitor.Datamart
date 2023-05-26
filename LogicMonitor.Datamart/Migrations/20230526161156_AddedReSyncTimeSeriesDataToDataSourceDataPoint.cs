3dede484dc36da725923d12fa024e69811c2f2ab
David Bond <david.bond@panoramicdata.com>
2023-05-26 17:12:55 +0100

    Sigh

--- LogicMonitor.Datamart/Migrations/20230526161156_AddedReSyncTimeSeriesDataToDataSourceDataPoint.cs
+++ LogicMonitor.Datamart/Migrations/20230526161156_AddedReSyncTimeSeriesDataToDataSourceDataPoint.cs
@@ -1,29 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
-
-#nullable disable
+﻿#nullable disable
 
 namespace LogicMonitor.Datamart.Migrations
 {
-    /// <inheritdoc />
-    public partial class AddedReSyncTimeSeriesDataToDataSourceDataPoint : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<bool>(
-                name: "ResyncTimeSeriesData",
-                table: "DataSourceDataPoints",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false);
-        }
+	/// <inheritdoc />
+	public partial class AddedReSyncTimeSeriesDataToDataSourceDataPoint : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AddColumn<bool>(
+				name: "ResyncTimeSeriesData",
+				table: "DataSourceDataPoints",
+				type: "boolean",
+				nullable: false,
+				defaultValue: false);
+		}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "ResyncTimeSeriesData",
-                table: "DataSourceDataPoints");
-        }
-    }
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropColumn(
+				name: "ResyncTimeSeriesData",
+				table: "DataSourceDataPoints");
+		}
+	}
 }
