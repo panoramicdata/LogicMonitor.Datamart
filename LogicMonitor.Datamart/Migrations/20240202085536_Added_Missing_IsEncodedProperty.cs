e0815d8bb300707091a9f9b89310b1a58255f60b
Roland Banks <roland.banks@panoramicdata.com>
2024-02-02 16:36:52 +0700

    Fixed missing migration for IsEncoded and OtelVerison.

---
+++ LogicMonitor.Datamart/Migrations/20240202085536_Added_Missing_IsEncodedProperty.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LogicMonitor.Datamart.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Missing_IsEncodedProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEncoded",
+                table: "Collectors",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtelVerison",
+                table: "Collectors",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEncoded",
+                table: "Collectors");
+
+            migrationBuilder.DropColumn(
+                name: "OtelVerison",
+                table: "Collectors");
+        }
+    }
+}
