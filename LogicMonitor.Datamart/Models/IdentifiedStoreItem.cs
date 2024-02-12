9afe1920c39f2453f700aa58fb42f6f983fec825
Roland Banks <roland.banks@panoramicdata.com>
2024-02-12 14:15:54 +0700

    Changed DatamartLastObserved temporarily to work in Thailand.

--- LogicMonitor.Datamart/Models/IdentifiedStoreItem.cs
+++ LogicMonitor.Datamart/Models/IdentifiedStoreItem.cs
@@ -10,5 +10,5 @@ public abstract class IdentifiedStoreItem : StoreItem
 	/// <summary>
 	/// When the Item was last observed
 	/// </summary>
-	public DateTimeOffset DatamartLastObserved { get; set; } = DateTimeOffset.MinValue;
+	public DateTimeOffset DatamartLastObserved { get; set; } = DateTimeOffset.UtcNow.AddYears(-100);
 }
