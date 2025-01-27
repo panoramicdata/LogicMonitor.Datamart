48b62978e2a8b653eef7df85ee07341b47891863
Roland Banks <roland.banks@panoramicdata.com>
2025-01-27 12:21:54 +0700

    Updated several nuget packages including LogicMonitor.Api.

--- LogicMonitor.Datamart/Mapping/AlertProfile.cs
+++ LogicMonitor.Datamart/Mapping/AlertProfile.cs
@@ -87,13 +87,14 @@ public class AlertProfile : Profile
 					}
 				)
 			)
-			.ForMember(dest => dest.Sdt, opts => opts.Ignore())
-			.ForMember(dest => dest.CustomColumns, opts => opts.Ignore())
-			.ForMember(dest => dest.MonitorObjectGroups, opts => opts.Ignore())
 			.ForMember(dest => dest.AlertGroupEntityValue, opts => opts.Ignore())
-			.ForMember(dest => dest.LogPartition, opts => opts.Ignore())
+			.ForMember(dest => dest.AlertQuery, opts => opts.Ignore())
+			.ForMember(dest => dest.CustomColumns, opts => opts.Ignore())
 			.ForMember(dest => dest.ExternalTicketUrl, opts => opts.Ignore())
+			.ForMember(dest => dest.LogPartition, opts => opts.Ignore())
 			.ForMember(dest => dest.LogMetadata, opts => opts.Ignore())
+			.ForMember(dest => dest.MonitorObjectGroups, opts => opts.Ignore())
+			.ForMember(dest => dest.Sdt, opts => opts.Ignore())
 			;
 	}
 }
